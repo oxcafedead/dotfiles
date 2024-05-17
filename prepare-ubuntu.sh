592715692e1fb329b54fd1c10f9adae0bf714067
Artem Vozhdayenko <artem_vozhdayenko@epam.com>
2024-05-17 16:21:17 +0300

    Add prepare script to ease installation on a new system

---
+++ prepare-ubuntu.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This is very rough script to prepare the environment for the dotfiles, probably will not work since
+# it was created retrospectively.
+
+# Update the package lists
+sudo apt-get update
+# Install curl and other needed utilities
+sudo apt-get install curl unzip xclip -y
+
+sudo add-apt-repository ppa:neovim-ppa/unstable
+source /etc/lsb-release
+echo "deb http://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/unstable/xUbuntu_${DISTRIB_RELEASE}/ /" | sudo tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list
+curl -L "https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_${DISTRIB_RELEASE}/Release.key" | sudo tee /etc/apt/trusted.gpg.d/devel_kubic_libcontainers_unstable.asc > /dev/null
+
+sudo apt-get update
+sudo apt-get install neovim git zsh ripgrep python3-pip podman -y
+
+# Install plugins manager for Neovim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+# Clone needed repositories for tmux plugins
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/tmux-plugins/tmux-resurrect ~/.tmux/plugins/tmux-resurrect
+git clone https://github.com/tmux-plugins/tmux-continuum ~/.tmux/plugins/tmux-continuum
+
+# Install Python needed modules
+python3 -m pip install autopep8 pylint
+
+# Change default shell to ZSH
+chsh -s $(which zsh)
+
+# Print done
+echo "Finished Installing. Re-login or reboot to use ZSH as your default shell. Then run ~/.dotfiles/dotinit.zsh"
